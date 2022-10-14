e02f680f47b37a2e7e86782809abff6ed46163a6
Zachary C <zcutting22@gmail.com>
2022-10-14 18:59:56 -0400

    Able to click on numbers and show results on display

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const solve = document.querySelector('.equate')
+const numbers = document.querySelectorAll('.num');
+let value = '';
+const store = [];
+
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        value += number.innerText;
+        solve.innerText = parseInt(value); 
+    })
+})
+
+//Clear the display
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    value = '';
+    solve.innerText = '';
+})
