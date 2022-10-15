d5cc1c82bab264a44282684c797c65b2c44efbfa
Zachary C <zcutting22@gmail.com>
2022-10-15 14:03:49 -0400

    Added basic math functions to script file

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+//Create functions for simple math operations.
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+//Create operate function that takes in an operator and 2 numbers that call the math operations.
