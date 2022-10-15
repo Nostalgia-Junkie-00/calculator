f6ccd9fd658c005515b3324d098fb23296a00eee
Zachary C <zcutting22@gmail.com>
2022-10-15 14:27:42 -0400

    Created operate function

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,23 @@ function divide(a, b){
     return a / b;
 }
 //Create operate function that takes in an operator and 2 numbers that call the math operations.
+function operate(operator, a, b){
+    switch(operator) {
+        case ('add'):
+            return add(a, b);
+            break
+        case 'subtract':
+            return subtract(a, b);
+            break
+        case 'multiply':
+            return multiply(a, b);
+            break
+        case 'divide':
+            return divide(a, b);
+            break
+        default:
+            return "hello"
+    }
+}
+
+console.log(operate('add', 10, 10));
