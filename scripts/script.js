40244226efc635cdca2b24cbf0270ee0252ae06d
Zachary C <zcutting22@gmail.com>
2022-10-16 14:44:02 -0400

    Finished operate function

--- scripts/script.js
+++ scripts/script.js
@@ -7,12 +7,19 @@ const clearButton = document.querySelector('.clear');
 
 //Variables
 let equation = '';
+let operation = '';
+let action = false;
+let a = 0;
+let b = 0;
 
 //Show numbers on display
 numButtons.forEach(number => {
     number.addEventListener('click', () => {
+        if(action === true){
+            action = false;
+        }
         equation += number.innerText;
-        display.innerText = equation;    
+        display.innerText = equation;     
     })
 })
 //Create functions for simple math operations.
@@ -31,59 +38,74 @@ function divide(a, b){
 //Create operate function that takes in an operator and 2 numbers that call the math operations.
 function operate(operator, a, b){
     switch(operator) {
-        case '+':
+        case 'add':
             return add(a, b);
             break
-        case '-':
+        case 'subtract':
             return subtract(a, b);
             break
-        case '*':
+        case 'multiply':
             return multiply(a, b);
             break
-        case '/':
+        case 'divide':
             return divide(a, b);
             break
         default:
             return "Not an operator"
     }
 }
+//Get operands from equation
+
 //Get operators from buttons
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
-        a = parseInt(equation);
-        operation = operator.id;
-        switch(operation) {
-            case ('add'):
-                equation+= '+';
-                break
-            case 'subtract':
-                equation+= '-';
-                break
-            case 'multiply':
-                equation+= '*';
-                break
-            case 'divide':
-                equation+= '/';
-                break
-            default:
-                return "Not an operator"
-        }
-        display.innerText = equation;    
+        if (equation === ''){
+            console.log('No values entered');
+        }else if(action === true){
+            console.log('Cannot perform another operation');
+        }else {
+            //If b exist, solve the problem first
+            if(equation.split(/[+*\/-]/g)[1] !== undefined){
+                solve();
+            }
+            a = parseInt(equation);
+            operation = operator.id;
+            switch(operation) {
+                case ('add'):
+                    equation+= '+';
+                    break
+                case 'subtract':
+                    equation+= '-';
+                    break
+                case 'multiply':
+                    equation+= '*';
+                    break
+                case 'divide':
+                    equation+= '/';
+                    break
+                default:
+                    return "Not an operator"
+            }
+            action = true;
+            display.innerText = equation; 
+        }  
     })
 })
 //Solve equation
 function solve(){
-    let index = equation.search(/[+*\/-]/g)
-    let operation = equation.substring(index, index + 1);
-    let [a, b] = equation.split(operation);
-    display.textContent = operate(operation, parseInt(a), parseInt(b));
-
+    b = parseInt(equation.split(/[+*\/-]/g)[1]);
+    equation = operate(operation, a, b);
+    display.innerText = equation;
 }
 
 equals.addEventListener('click', solve);
 //Clear
 function clear(){
     equation = '';
+    operation = '';
+    action = false;
+    a = 0;
+    b = 0;
     display.innerText = equation;
 }
 
